34187c0924002b728a1f79f4e47c356af1050566
plas12 <143487328+plas12@users.noreply.github.com>
2024-11-26 17:28:10 +0900

    Add files via upload

---
+++ 241126.cpp
@@ -0,0 +1,60 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <utility>
+#include <stack>
+#include <queue> 
+
+using namespace std;
+
+
+
+
+
+int main(){
+	
+	//순서대로 int형정렬 코드
+	int a = 0;
+	vector <int> v;
+	cin >> a;
+	
+	for(int i = 0; i < a; i++){
+		int n = 0;
+		cin >> n;
+		v.push_back(n);
+	}
+	
+	int i = 1, x = 0, y = 0;
+	
+	while(1){
+		if(i == v.size()){
+			for(int f = 0; f < v.size(); f++){
+				printf("%d ", v[f]);
+			}
+			break;
+		}
+		
+		x = v[i - 1];
+		y = v[i];
+		
+
+		if(x > y){
+			int n = x;
+			for(int j = i; j < v.size(); j++){
+				v[j - 1] = v[j];
+			}
+			v[v.size() - 1] = x;
+			i = 1;
+		}
+		else{
+			i++;
+		}
+		
+	}
+	
+	
+	
+	return 0;
+}
+
