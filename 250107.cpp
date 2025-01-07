f0282ec712bbb580fc6879447500b92bfa2297fa
plas12 <143487328+plas12@users.noreply.github.com>
2025-01-07 18:04:21 +0900

    Add files via upload

---
+++ 250107.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <utility>
+#include <stack>
+#include <queue> 
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+
+
+
+
+
+int main(){
+	
+	//1(2502
+	int l = 0, n;
+	int a[31];
+	
+	cin >> l >> n;
+
+	a[l] = n;
+	
+	int k = l;
+	while(1){
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
