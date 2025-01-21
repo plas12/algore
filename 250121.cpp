66fbf0edc4e79db0f82130bbd06ce5f69dcff018
plas12 <143487328+plas12@users.noreply.github.com>
2025-01-21 18:08:17 +0900

    Add files via upload

---
+++ 250121.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <utility>
+#include <stack>
+#include <queue> 
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+//2(1912
+int a[100001];
+
+
+
+int main(){
+	/*
+	//1(2156
+	int l = 0;
+	int dp[10001];
+	int a[10001];
+	cin >> l;
+	
+	for(int i = 0; i < l; i++){
+		cin >> a[i];
+	}
+	
+	dp[0] = a[0];
+	dp[1] = a[0] + a[1];
+	dp[2] = max(a[0] + a[1], max(a[1] + a[2], a[0] + a[2]));
+	//printf("%d\n%d\n%d\n", dp[0], dp[1], dp[2]);
+	
+	for(int i = 3; i < l; i++){
+		dp[i] = max(dp[i - 3] + a[i - 1] + a[i], max(dp[i - 2] + a[i], dp[i - 1]));
+		//printf("%d %d\n", dp[i], max(dp[i - 3] + a[i - 1] + a[i], max(dp[i - 2] + a[i], dp[i - 1])));
+	}
+	
+	printf("%d", dp[l - 1]);
+	*/
+	
+	
+	//2(1912
+	int l = 0;
+	cin >> l;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+}
