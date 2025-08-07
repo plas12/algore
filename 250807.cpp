aff53d6b2f5c83ed57179a0f0e5e9f6541f94049
plas12 <143487328+plas12@users.noreply.github.com>
2025-08-07 20:56:22 +0900

    Add files via upload

---
+++ 250807.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <iostream>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+/*
+//nypc 등차수열 
+int N = 0;
+vector <int> v;
+vector <int> a;
+*/
+
+
+int main(){
+	/*
+	//nypc 등차수열 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        int dummy;
+        cin >> dummy;
+
+        v.push_back(dummy);
+		
+		if(i != 0){
+			if(abs(v[i] - v[i - 1]) != 1){
+          	  a.push_back(i);
+       	 	}
+		}
+    }
+
+    if(a.size() == 1){
+        int result = min(a[0], N - a[0]);
+
+        printf("%d", result);
+    }
+    else{
+        int result = a[0] + (N - a[1]);
+
+        printf("%d", result);
+    }
+    */
+
+	
+    
+
+    return 0;
+}
