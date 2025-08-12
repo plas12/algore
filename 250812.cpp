92f801aabc9f033cef2410abcfcc5090abbc1abd
plas12 <143487328+plas12@users.noreply.github.com>
2025-08-12 18:57:54 +0900

    Add files via upload

---
+++ 250812.cpp
@@ -0,0 +1,106 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+/*
+//비코 2024년도 2 - A 
+int N;
+vector <int> U;
+vector <int> D;
+string str = "";
+int n = 0;
+*/
+
+
+int N = 0;
+int map[]
+
+int main(){
+	/*
+	
+	//비코 2024년도 2 - A 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        int dummy = 0;
+        cin >> dummy;
+
+        U.push_back(dummy);
+    }
+
+    for(int i = 0; i < N; i++){
+        int dummy = 0;
+        cin >> dummy;
+        
+    	n += dummy;
+
+        D.push_back(dummy);
+    }
+
+    for(int i = 0; i < N; i++){
+    	if(U[i] <= n - D[i]){
+    		if(U[i] == -1){
+//    			printf("! ");
+    			
+    			str += "0";
+    				
+    			n -= D[i];
+			}
+			else{
+//				printf("@ ");
+				
+				n -= U[i] + D[i];
+    		
+    			str += "+";	
+			}
+		}
+		else{
+			if(D[i] == -1){
+//				printf("# ");
+				
+				str += "0";
+			}
+			else{
+				if(i == 0){
+//					printf("$ ");
+					
+					str += "0";
+					
+					n -= D[i];
+				}
+				else{
+//					printf("% ");
+				
+					str += "-";	
+				}
+			}
+		}
+    }
+//    printf("\n\n");
+    
+    cout << str;
+    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
